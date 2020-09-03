a636027c4f67be8bbd571e4e7552982b91ab0080
Bilal A <bilo0765@stud.kea.dk>
2020-09-03 11:20:05 +0200

    ikke ses

--- delfinen/src/demo/Main.java
+++ delfinen/src/demo/Main.java
@@ -3,7 +3,7 @@ package demo;
 public class Main {
 
     public static void main(String[] args) {
-        System.outprintnl("hey")
+        System.outprintnl("ikke ses")
 	// write your code here
     }
 }
